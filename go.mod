f0bedc86be76fd12edc45365141ec84d5c05e548
Amílcar Sánchez <amilcaralex97@gmail.com>
2021-11-12 15:05:39 -0600

    Server initialized

---
+++ go.mod
@@ -0,0 +1,5 @@
+module academy-go-q42021
+
+go 1.17
+
+require github.com/go-chi/chi/v5 v5.0.5 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello world")	
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	http.ListenAndServe(":3000", r)
 }
