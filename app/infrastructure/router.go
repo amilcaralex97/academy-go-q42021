8653c31bfb1f06c0e8ea3d8b3c44978205293d68
Amílcar Sánchez <amilcaralex97@gmail.com>
2021-11-12 18:14:03 -0600

    server initialized

---
+++ app/infrastructure/router.go
@@ -0,0 +1,24 @@
+package infrastructure
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// Dispatch is handle routing
+func Dispatch() {
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Printf("%s", err)
+	}
+}
