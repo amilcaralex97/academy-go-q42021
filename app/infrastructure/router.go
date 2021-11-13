0fedc8868a5337cc71f1997aef0a04462eb9b3da
Amílcar Sánchez <amilcaralex97@gmail.com>
2021-11-12 22:22:15 -0600

    routes implemented

--- app/infrastructure/router.go
+++ app/infrastructure/router.go
@@ -4,19 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"go-project/app/interfaces"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 // Dispatch is handle routing
 func Dispatch() {
-
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+	
+	r.Get("/characters", interfaces.Index)
+	r.Get("/characters/:id", interfaces.Show)
 
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Printf("%s", err)
