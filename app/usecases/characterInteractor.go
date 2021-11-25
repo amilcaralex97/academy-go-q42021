08e6509de8e06110633f51949d19fde547eebb06
Amílcar Sánchez <amilcaralex97@gmail.com>
2021-11-24 21:42:54 -0600

    Error handling on interactor

--- app/usecases/characterInteractor.go
+++ app/usecases/characterInteractor.go
@@ -1,19 +1,28 @@
 package usecases
 
 import (
+	"errors"
 	"go-project/app/domain"
 )
 
 // Return all characters
-func Index() (characters domain.Characters) {
-	characters  = FindAll()
+func Index() (characters *domain.Characters, err error) {
+	characters, err  = FindAll()
+
+	if err != nil {
+		return nil, errors.New("error while trying to obtain characters")
+	}
 
 	return
 }
 
 // Return character by ID
-func Show(characterID int) (character domain.Characters){
-	character = FindByID(characterID)
+func Show(characterID int) (character *domain.Character, err error){
+	character, err = FindByID(characterID)
+
+		if err != nil {
+		return nil, errors.New("error while trying to obtain character")
+	}
 
 	return
 }
