fe3041e9444e28feb3e68b6c92003513d9afa487
Amilcar Sanchez <amillcar.wizeline.com>
2021-11-29 23:05:01 -0600

    character repository fetch

--- app/usecases/characterRepository.go
+++ app/usecases/characterRepository.go
@@ -1,20 +1,41 @@
 package usecases
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"net/http"
 
 	"go-project/app/common"
 	"go-project/app/domain"
 )
 
-const CSVFile string = "/home/amilcar/Documents/Projects/go-project/app/resources/characters.csv"
-const CsvError string = "error while trying to read CSV file"
+const CSVFile string = "/Users/alejandrosanchez/Documents/go_bootcamp/app/resources/characters.csv"
 
 type CharactersRepo struct{}
 
-
-func NewCharacterRepo() CharactersRepo{
+func NewCharacterRepo() CharactersRepo {
 	return CharactersRepo{}
+}
+
+func (CharactersRepo) FetchCharacters() (*domain.Characters, error) {
+	resp, err := http.Get("https://swapi.dev/api/people")
+
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	//Create a variable of the same type as our model
+	var cResp *domain.Characters
+
+	json_resp := json.NewDecoder(resp.Body).Decode(&cResp)
+
+	fmt.Println(json_resp)
+
+	//Invoke the text output function & return it with nil as the error value
+	return nil, json_resp
 }
 
 func (CharactersRepo) FindAll() (*domain.Characters, error) {
@@ -41,7 +62,7 @@ func (CharactersRepo) FindByID(characterID int) (*domain.Character, error) {
 	character := domain.Character{}
 
 	for _, v := range characterList {
-		if(v.ID == characterID){
+		if v.ID == characterID {
 			character = v
 		}
 	}
