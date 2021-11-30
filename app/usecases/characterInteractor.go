427dbe606f19e08f471443db63c2bd26a8dc9ca8
Amilcar Sanchez <amillcar.wizeline.com>
2021-11-29 23:05:27 -0600

    character interactor fetch interactor

--- app/usecases/characterInteractor.go
+++ app/usecases/characterInteractor.go
@@ -6,22 +6,30 @@ import (
 	"go-project/app/domain"
 )
 
-type getter interface{
+type getter interface {
 	FindAll() (*domain.Characters, error)
 	FindByID(characterID int) (*domain.Character, error)
+	FetchCharacters() (*domain.Characters, error)
 }
 
 type CharactersInteractor struct {
-	 repo getter
+	repo getter
 }
 
-func NewCharactersInteractor(repo CharactersRepo) CharactersInteractor{
+func NewCharactersInteractor(repo CharactersRepo) CharactersInteractor {
 	return CharactersInteractor{repo}
+}
+
+//Return fetched characters
+func (ci CharactersInteractor) FetchCharacters() (characters *domain.Characters, err error) {
+	characters, err = ci.repo.FetchCharacters()
+
+	return
 }
 
 // Return all characters
 func (ci CharactersInteractor) Index() (characters *domain.Characters, err error) {
-	characters, err  = ci.repo.FindAll()
+	characters, err = ci.repo.FindAll()
 
 	if err != nil {
 		return nil, errors.New(err.Error())
@@ -31,13 +39,12 @@ func (ci CharactersInteractor) Index() (characters *domain.Characters, err error
 }
 
 // Return character by ID
-func (ci CharactersInteractor) Show(characterID int) (character *domain.Character, err error){
+func (ci CharactersInteractor) Show(characterID int) (character *domain.Character, err error) {
 	character, err = ci.repo.FindByID(characterID)
 
-		if err != nil {
+	if err != nil {
 		return nil, errors.New(err.Error())
 	}
 
 	return
 }
-
